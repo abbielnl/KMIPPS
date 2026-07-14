4ba1c0edfcfa311500cdd380a2b24eff36d76809
Abbie Kirimi <abbiekirimi@gmail.com>
2026-07-14 13:09:53 +0300

    Prepare app for Render deployment

--- db.py
+++ db.py
@@ -1,12 +1,20 @@
+import os
 import psycopg2
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="kmipps",
-    user="postgres",
-    password="1qaZ2ws$",
-    port="5432"
-)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # Running on Render
+    conn = psycopg2.connect(DATABASE_URL)
+else:
+    # Running locally
+    conn = psycopg2.connect(
+        host="localhost",
+        database="kmipps",
+        user="postgres",
+        password="1qaZ2ws$",
+        port="5432"
+    )
 
 cursor = conn.cursor()
 
